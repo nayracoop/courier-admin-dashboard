d6f5103e5fdb9cbefc1e9dc843d376dc5db0b0bf
Pehuen Rodriguez <pehuen.rodriguez@gmail.com>
2018-07-12 16:20:16 -0300

    cambios en los archivos de configuración de webpack

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  PARSE_URI: '"http://localhost:1337/parse"',
+  PARSE_URI: '"https://staging.nayra.coop/ca-parse/parse"',
   PARSE_APP_ID: '"COURIER_ADMIN"',
   PARSE_JAVASCRIPT_KEY: '"***REMOVED***"',
   PARSE_MASTER_KEY: '"***REMOVED***"'
