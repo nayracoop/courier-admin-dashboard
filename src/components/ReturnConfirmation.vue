cf0baff9efaaeab8d966b36dbbc771f21075e8be
Pehuen Rodriguez <pehuen.rodriguez@gmail.com>
2018-08-01 14:44:35 -0300

    unificación del modal. Control de botón volver e importación de listas de costso

---
+++ src/components/ReturnConfirmation.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-modal id="modal-confirm" ref="deleteModal" hide-footer centered title="Confirmación">
+    <p class="my-2 mb-4 text-center">El registro fue modificado ¿Desea volver atrás y perder los cambios?</p>
+    <b-row>
+      <b-col sm="12 text-center" slot="modal-footer">
+        <b-button variant="primary" @click="onCancel">No, deseo continuar editando</b-button>
+        <b-button variant="danger" @click="onConfirm">Sí, descarto los cambios</b-button>
+      </b-col>
+    </b-row>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: 'c-return-confirmation-modal',
+  methods: {
+    onConfirm () {
+      this.$emit('confirmReturn')
+    },
+    onCancel () {
+      this.$emit('cancelReturn')
+    }
+  }
+}
+</script>
