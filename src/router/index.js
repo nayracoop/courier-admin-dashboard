852e549742a39aa1f6deb26af45526aad71ba808
Ignacio Guerra <ignacio.guerra@nayra.coop>
2018-11-01 12:53:47 -0300

    Fix de url base

--- src/router/index.js
+++ src/router/index.js
@@ -72,7 +72,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: String(process.env.BASE_URL),
   linkActiveClass: 'open active',
   scrollBehavior: () => ({ y: 0 }),
   routes: [
