b71e0b24009bf726a59b6866ccdd5ca75dfc0d1b
Pehuen Rodriguez <pehuen.rodriguez@gmail.com>
2018-12-17 23:45:36 -0300

    Fixed errors vv-as

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -13,7 +13,7 @@
         <b-form-group>
           <label for="name">Email</label>
           <i class="fa fa-question-circle fa-sm" v-b-tooltip.hover title="Email del usuario"></i>
-          <b-form-input :class="{ danger: errors.has('email') }" v-validate="'required'" name="email" data-vv-as="nombre" type="text" id="email" v-model="user.email" placeholder="Ej: juan@email.com"></b-form-input>
+          <b-form-input :class="{ danger: errors.has('email') }" v-validate="'required'" name="email" data-vv-as="email" type="text" id="email" v-model="user.email" placeholder="Ej: juan@email.com"></b-form-input>
           <span><small class="inv-feedback" v-show="errors.has('email')">{{ errors.first('email') }}</small></span>
         </b-form-group>
       </b-col>
@@ -30,7 +30,7 @@
         <b-form-group>
           <label for="name">Password</label>
           <i class="fa fa-question-circle fa-sm" v-b-tooltip.hover title="Password para el usuario"></i>
-          <b-form-input :class="{ danger: errors.has('password') }" v-validate="'required'" name="password" data-vv-as="nombre" type="password" id="password" v-model="user.password" placeholder="Ej: password123"></b-form-input>
+          <b-form-input :class="{ danger: errors.has('password') }" v-validate="'required'" name="password" data-vv-as="password" type="password" id="password" v-model="user.password" placeholder="Ej: password123"></b-form-input>
         </b-form-group>
       </b-col>
     </b-form-row>
