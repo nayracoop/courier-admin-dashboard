e3e2874554de9e7fabfab97f9df62522e00c2811
Ignacio <ignacio.guerra@nayra.coop>
2018-12-28 15:46:36 -0300

    New prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,10 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  PARSE_URI: '"https://staging.nayra.coop/ca-parse/parse"',
+  // PARSE_URI: '"https://staging.nayra.coop/ca-parse/parse"',
+  PARSE_URI: '"***REMOVED***"',
   PARSE_APP_ID: '"COURIER_ADMIN"',
   PARSE_JAVASCRIPT_KEY: '"***REMOVED***"',
-  BASE_URL: '"/courieradmin//"'
+  // BASE_URL: '"/courieradmin//"'
+  BASE_URL: '""'
 }
