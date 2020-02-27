a3c19fef3a73dd297a5cd6128401a00caefa76d8
Pehuén <pehuen.rodriguez@nayra.coop>
2020-02-27 19:45:00 -0300

    Clean URLs and KEYS to patch them

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // PARSE_URI: '"https://staging.nayra.coop/ca-parse/parse"',
-  PARSE_URI: '"***REMOVED***"',
+  // PARSE_URI: '""',
+  PARSE_URI: '""',
   PARSE_APP_ID: '"COURIER_ADMIN"',
-  PARSE_JAVASCRIPT_KEY: '"***REMOVED***"',
+  PARSE_JAVASCRIPT_KEY: '""',
   // BASE_URL: '"/courieradmin//"'
   BASE_URL: '"/"'
 }
